5790f1a02c14ebec84d60c0d66d001fe64dc3926
Dujanah Zayn <dujanah.hasan@gmail.com>
2023-12-01 03:24:13 -0500

    Modified AndroidManifest.xml, announcement_creation_view.xml, CreateAnnouncementView.java and strings.xml

--- app/src/main/java/com/example/b07_group_19/CreateAnnouncementView.java
+++ app/src/main/java/com/example/b07_group_19/CreateAnnouncementView.java
@@ -4,22 +4,59 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAnnouncementView extends AppCompatActivity {
+    private EditText title;
+    private EditText message;
+    private Button publishAnnouncement;
+    private Button cancelAnnouncement;
+
+    private CreateAnnouncementPresenter presenter;
+
+
+    public void returnHome(){
+        Intent intent = new Intent(CreateAnnouncementView.this, AdminHomeActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.announcement_creation_view);
 
-        Button cancelAnnouncement = findViewById(R.id.cancel_announcement_button);
+        cancelAnnouncement = findViewById(R.id.cancel_announcement_button);
         cancelAnnouncement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CreateAnnouncementView.this, AdminHomeActivity.class);
-                startActivity(intent);
+                returnHome();
             }
         });
+
+        publishAnnouncement = findViewById(R.id.publish_announcement_button);
+        title = findViewById(R.id.announcementTitleInput);
+        message = findViewById(R.id.messageInput);
+
+        presenter = new CreateAnnouncementPresenter(new CreateAnnouncementModel(), this);
+        publishAnnouncement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Announcement announcement = new Announcement();
+                announcement.setTitle(title.getText().toString());
+                announcement.setMessage(message.getText().toString());
+                presenter.publishAnnouncement(announcement);
+            }
+        });
+    }
+
+    public void showAnnouncementSuccessful(){
+        Intent intent = new Intent(CreateAnnouncementView.this, SuccessfulAnnouncementActivity.class);
+        startActivity(intent);
+    }
+
+    public void publishAnnouncement(Announcement announcement){
+        showAnnouncementSuccessful();
     }
 }
