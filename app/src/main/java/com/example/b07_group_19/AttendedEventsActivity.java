a7d62eddbf28ea05274b24d3cba76e59ea7b2629
scientistbodybuilder <o.jikineh@mail.utoronto.ca>
2023-12-04 12:07:58 -0500

    added US 6 event files

---
+++ app/src/main/java/com/example/b07_group_19/AttendedEventsActivity.java
@@ -0,0 +1,14 @@
+package com.example.b07_group_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttendedEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attended_events);
+    }
+}
