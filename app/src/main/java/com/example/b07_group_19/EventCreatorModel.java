cb1466e9b6c0014ab43a303954cc127659284b25
scientistbodybuilder <o.jikineh@mail.utoronto.ca>
2023-12-05 14:20:56 -0500

    Resolved possible merge conflicts for gradle.build.kts and AndroidManifest.xml and PostReqActivity

--- app/src/main/java/com/example/b07_group_19/EventCreatorModel.java
+++ app/src/main/java/com/example/b07_group_19/EventCreatorModel.java
@@ -5,6 +5,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
