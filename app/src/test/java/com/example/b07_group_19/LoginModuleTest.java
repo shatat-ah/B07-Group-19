249ee74f00b646bea51d20f0ef3507a4948f6980
scientistbodybuilder <o.jikineh@mail.utoronto.ca>
2023-12-05 23:13:21 -0500

    Updated UI of US9 xml files

--- app/src/test/java/com/example/b07_group_19/LoginModuleTest.java
+++ app/src/test/java/com/example/b07_group_19/LoginModuleTest.java
@@ -1,19 +1,15 @@
 package com.example.b07_group_19;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -21,7 +17,7 @@ import static org.mockito.Mockito.when;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(MockitoJUnitRunner.class)
-public class ExampleUnitTest {
+public class LoginModuleTest {
 
     @Mock
     LoginActivityView View;
